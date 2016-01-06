8d70d5a5cbdc88df30c68e03dc4eb029535c9408
Qingyang Li <congel@gmail.com>
2016-01-06 00:11:29 -0500

    renamed draw() method to drawMarker()

--- src/module5/CityMarker.java
+++ src/module5/CityMarker.java
@@ -35,7 +35,8 @@ public class CityMarker extends CommonMarker {
 	/**
 	 * Implementation of method to draw marker on the map.
 	 */
-	public void draw(PGraphics pg, float x, float y) {
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
 		// Save previous drawing style
 		pg.pushStyle();
 		
@@ -78,9 +79,5 @@ public class CityMarker extends CommonMarker {
 	}
 
 
-	@Override
-	public void drawMarker(PGraphics pg, float x, float y) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
